cf47b8051d69a6f15a29f1718816a339202dbbd6
Joel Lubrano <joel.lubrano@gmail.com>
2017-01-30 21:03:13 -0500

    Wire up the WebSocket

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,35 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      User.new(cookies[:name])
+    end
+  end
+
+  class User
+    def initialize(name)
+      @name = name
+    end
+
+    attr_reader :name
+
+    def appear(on: nil)
+      puts "#{@name} appearing on #{on}"
+    end
+
+    def away
+      puts "#{@name} away"
+    end
+
+    def disappear
+      puts "#{@name} disappear"
+    end
   end
 end
